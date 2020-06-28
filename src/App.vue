ad475c10814a2e419ac8582b2805003c1414eedd
uttues <458594033@qq.com>
2020-06-28 10:10:04 +0800

    6.28 按钮切换主题

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <div id="app">
         <Header />
         <router-view />
+        <button class="btn-transfer" @click="testTransferTheme">主题切换</button>
     </div>
 </template>
 
@@ -12,6 +13,20 @@ export default {
     name: "App",
     components: {
         Header
+    },
+    data() {
+        return {
+            theme: 'day'
+        }
+    },
+    methods: {
+        testTransferTheme(){
+            this.theme = this.theme === 'day' ? 'night' : 'day'
+            document.documentElement.setAttribute(
+                "data-theme",
+                this.theme,
+            );
+        }
     }
 };
 </script>
--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -12,21 +12,11 @@
 
 <script>
 import Nav from './sub-comps/nav'
-
 export default {
     components: {
         Nav
     }
 };
-// light
-// bg #ffffff
-// #4f4f4f active
-// #828282
-
-// dark
-// bg #1a1e21
-// #898989 active
-// #27262b
 </script>
 
 <style lang="scss" scoped>
