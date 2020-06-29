7536cdee9d1ae4a48ea167766da4893e7ed894b1
uttues <458594033@qq.com>
2020-06-29 14:06:30 +0800

    6.29 发现页面-接口整理1

--- src/apis/page-api/find/config.js
+++ src/apis/page-api/find/config.js
@@ -2,19 +2,28 @@ import base from 'apis/base'
 
 const baseURL = base.neteaseCloudDev
 
-export const bannerSwiperList = baseURL + '/banner' // 轮播图
+export const bannerSwiperList = baseURL + '/banner' // banner( 轮播图 ) 数据
 
-// export const recSongList = baseURL + '/top/playlist' // 推荐歌单，歌单广场
+export const selectedSongLists = baseURL + '/top/playlist' // 网友精选碟歌单(icons下面一个module) => 推荐歌单，歌单广场
+
+// 根据歌单分类给'网友精选碟歌单'随机添加类别参数，将请求的歌单列表随机展示
+export const songListCategory = baseURL + '/playlist/catlist' // 歌单分类,包含 category 信息
+
+export const newDish = baseURL + '/top/album' // 新碟上架列表
+export const newSongs = baseURL + '/top/song' // 新歌速递
+
+
+
+
+// export const dailyRecommendSongs = baseURL + '/recommend/songs' // 每日推荐歌曲（需要登录）
+// export const dailyRecommendSongLists = baseURL + '/recommend/resource' // 每日推荐歌单（需要登录）【发现页展示的那六个】
+
 // export const highquality = baseURL + '/top/playlist/highquality' // 精品歌单
-// export const catlist = baseURL + '/playlist/catlist' // 获取歌单分类 !!!
-// export const hot = baseURL + '/playlist/hot' // 获取热门歌单分类 !!!
-// export const topList = baseURL + '/toplist/detail' // 获取所有榜单内容摘要
-// export const idxList = baseURL + '/top/list' // 获取排行榜
-// export const albumDetail = baseURL + '/playlist/detail' // 获取歌单详情
-// export const recSongs = baseURL + '/recommend/songs' // 每日推荐歌曲
-// export const dateRecSongList = baseURL + '/recommend/resource' // 每日推荐歌单，发现页展示的那六个
-// export const newDish = baseURL + '/top/album' // 发现页新碟
-// export const getDishInfo = baseURL + '/album' // 获取专辑内容
-// export const newSongs = baseURL + '/top/song' // 发现页新歌
-// export const personalFm = baseURL + '/personal_fm' // 发现页私人FM
+// export const hot = baseURL + '/playlist/hot' // 热门歌单分类,包含 category 信息
+// export const topList = baseURL + '/toplist/detail' // 所有榜单内容摘要
+// export const idxList = baseURL + '/top/list' // 排行榜数据（传入榜单 id）
+// export const albumDetail = baseURL + '/playlist/detail' // 获取歌单详情（传入歌单 id）
+
+// export const DishInfo = baseURL + '/album' // 获取专辑内容（传入专辑 id）
+// export const personalFm = baseURL + '/personal_fm' // 私人FM（需要登录）
 
--- src/apis/page-api/find/index.js
+++ src/apis/page-api/find/index.js
@@ -1,14 +1,58 @@
 import axios from 'apis/axios'
 
 import {
-    bannerSwiperList
+    bannerSwiperList,
+    selectedSongLists,
+    songListCategory,
+    newDish,
+    newSongs
 } from './config'
 
 export default {
     /**
      * 发现页面-首页轮播图（返回列表）
+     * https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=banner
+     * @param{Number}type 资源类型，默认为 0 即PC
      */
-    getBannerSwiperList() {
-        return axios.get(bannerSwiperList)
-    }
+    getBannerSwiperList(type = 0) {
+        // 怎么传递参数？？
+        console.log('getBannerSwiperList接口被调用了：' + bannerSwiperList + `type=${type}`);
+        return axios.get(bannerSwiperList + `type=${type}`)
+    },
+
+    /**
+     * 发现页面-icon列表下面的，精选歌单（返回列表）
+     * https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=歌单-网友精选碟-
+     * @param{string}cat  tag, 比如 " 华语 "、" 古风 " 、" 欧美 "、" 流行 ", 默认为 "全部"
+     * @param{number}limit 取出歌单数量 , 默认为 20
+     * @param{number}before 分页参数，取上一页最后一个歌单的 updateTime 获取下一页数据
+     */
+    getSelectedSongLists(cat = '全部', limit = 20, before) {
+        return axios.get(selectedSongLists)
+    },
+
+    /**
+    * 获取歌单分类,包含 category 信息
+    */
+    getSongListCategory() {
+        return axios.get(songListCategory)
+    },
+
+
+    /**
+     * 发现页面-新碟上架
+     * @param{number}limit 取出数量 , 默认为 50
+     * @param{number}offset 偏移数量 , 用于分页 , 如 :( 页数 -1)*50, 其中 50 为 limit 的值 , 默认 为 0
+     */
+    getNewDish(limit = 50, offset = 0) {
+        return axios.get(newDish)
+    },
+
+    /**
+    * 发现页面-新歌速递
+    * @param{Number}type 歌曲类型id，详情见文档
+    */
+    getNewSongs(type) {
+        return axios.get(newSongs)
+    },
 }
