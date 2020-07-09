752c30a14edd68cca8552235f9d9e9a4a63372f0
uttues <458594033@qq.com>
2020-07-09 17:31:32 +0800

    7.9 歌曲item WIP

---
+++ src/components/rectangle-card/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="retangle-card-wrapper">
+    <img
+      :src="imgUrl"
+      alt
+    />
+    <div class="song-info-wrapper">
+      <p>
+        <span>{{ songName }}</span>
+        <span> - {{ singer }}</span>
+      </p>
+      <p>
+        <span>{{ label }}</span>
+        <span>{{ description }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgUrl: String,
+    songName: String,
+    singer: String,
+    label: String,
+    description: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./style.scss";
+</style>
