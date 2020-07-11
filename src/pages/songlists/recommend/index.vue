1a4bfcdb511d0af6446848faddd4e243fb107cd3
uttues <458594033@qq.com>
2020-07-11 12:27:03 +0800

    7.11 歌单广场页面-歌单容器组件

--- src/pages/songlists/recommend/index.vue
+++ src/pages/songlists/recommend/index.vue
@@ -1,14 +1,40 @@
 <template>
-   <div>
-      推荐
-   </div>
+  <div>
+    <div class=songlists-container>
+      <SongListItem
+        class="songlist-card"
+        v-for="item in songLists"
+        :key="item.id"
+        :playCount="item.playCount"
+        :name="item.name"
+        :imgUrl="item.coverImgUrl"
+        size="2.14rem"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import apis from "apis";
+import SongListItem from "components/square-card";
 export default {
-}
+  data() {
+    return {
+      songLists: []
+    };
+  },
+  components: {
+    SongListItem
+  },
+  mounted() {
+    apis.find.getSelectedSonglists().then(res => {
+      // 补充洗牌算法
+      this.songLists = res.data.playlists;
+    });
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-@import './style.scss'
+@import "./style.scss";
 </style>
