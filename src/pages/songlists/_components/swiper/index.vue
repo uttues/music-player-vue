816afd93461db0b81eb3c9797dc9fa1b961825dd
uttues <458594033@qq.com>
2020-07-11 13:19:39 +0800

    7.12 歌单广场页面引入轮播图（ 展示良好）

---
+++ src/pages/songlists/_components/swiper/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="banner-swiper-container">
+    <Swiper
+      height='5rem'
+      show-arrow-type="never"
+      handle-indication-type="hover"
+      :drag-ratio-min-limit="0.5"
+      :loop="true"
+      :autoplay="true"
+      mode-type="card"
+    >
+      <SwiperItem
+        v-for="(item, index) in list"
+        :key="index"
+      >
+        <a class="banner-item">
+          <SongListItem
+            class="songlist-card"
+            :key="item.id"
+            :playCount="item.playCount"
+            :name="item.name"
+            :imgUrl="item.coverImgUrl"
+            size="100%"
+          />
+        </a>
+      </SwiperItem>
+    </Swiper>
+  </div>
+</template>
+
+<script>
+import apis from "apis";
+import SongListItem from "components/square-card";
+import { Swiper, SwiperItem } from "components/swiper-card";
+export default {
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      banners: []
+    };
+  },
+  components: {
+    Swiper,
+    SwiperItem,
+    SongListItem
+  },
+  methods: {
+    MarkBgColor(color) {
+      switch (color) {
+        case "blue":
+          return "rgb(50, 147, 221, .95)";
+        case "red":
+          return "rgb(231, 67, 67, .95)";
+        default:
+          return "rgb(255, 255, 255, .95)";
+      }
+    }
+  },
+  mounted() {
+    apis.find.getBannerSwiperList().then(res => {
+      this.banners = res.data.banners;
+    });
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./style.scss";
+</style>
--- src/pages/songlists/recommend/index.vue
+++ src/pages/songlists/recommend/index.vue
@@ -1,9 +1,14 @@
+<!-- 后期把这个组件抽成公用的page -->
 <template>
   <div>
+    <TopSwiper
+      v-if="showSongLists"
+      :list="swiperSongLists"
+    />
     <div class=songlists-container>
       <SongListItem
         class="songlist-card"
-        v-for="item in songLists"
+        v-for="item in showSongLists"
         :key="item.id"
         :playCount="item.playCount"
         :name="item.name"
@@ -16,20 +21,35 @@
 
 <script>
 import apis from "apis";
+import TopSwiper from "../_components/swiper";
 import SongListItem from "components/square-card";
 export default {
   data() {
     return {
-      songLists: []
+      swiperSongLists: [],
+      showSongLists: []
     };
   },
+  computed: {
+    isShowSwiper() {
+      return this.$route.path.indexOf("recommend") !== -1;
+    }
+  },
   components: {
+    TopSwiper,
     SongListItem
   },
   mounted() {
+    // 一次请求多少条？
     apis.find.getSelectedSonglists().then(res => {
       // 补充洗牌算法
-      this.songLists = res.data.playlists;
+      if (this.isShowSwiper) {
+        this.swiperSongLists = res.data.playlists.slice(0, 3);
+        console.log(this.swiperSongLists);
+        this.showSongLists = res.data.playlists.slice(3);
+      } else {
+        this.showSongLists = res.data.playlists;
+      }
     });
   }
 };
